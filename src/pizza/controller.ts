b88a29559b6b8f0f845d55cb9f9d680e4bfe86ed
ghvw <ghvanw283@gmail.com>
2018-10-14 00:04:45 -0500

    update pizza controller with new base dao

--- src/pizza/controller.ts
+++ src/pizza/controller.ts
@@ -1,11 +1,40 @@
-import { Response } from "express";
-import { default as Pizza, PizzaModel } from "./model";
+import { Request, Response } from "express";
+import { PizzaModel } from "./model";
+import { IBaseDao } from "src/dao";
 
-export const findAll = (res: Response) => {
-  Pizza.find({}, (err, pizzas: PizzaModel[]) => {
-    if (err) {
-      res.status(500).send(`Error retrieving pizzas: ${err}`);
-    }
-    res.status(200).send(pizzas);
-  });
+// export const findAll = (res: Response) => {
+//   Pizza.find({}, (err, pizzas: PizzaModel[]) => {
+//     if (err) {
+//       res.status(500).send(`Error retrieving pizzas: ${err}`);
+//     }
+//     res.status(200).send(pizzas);
+//   });
+// }
+
+export class PizzaController {
+  baseDao: IBaseDao<PizzaModel>;
+
+  constructor(baseDao: IBaseDao<PizzaModel>) {
+    this.baseDao = baseDao;
+  }
+
+  getAllPizzas(res: Response): void {
+    this.baseDao.findAll()
+      .then(pizzas => {
+        res.status(200).send(pizzas);
+      })
+      .catch(err => {
+        res.status(500).send(`Error retrieving Pizzas: ${err}`);
+      });
+  }
+
+  getPizzaById(req: Request, res: Response): void {
+    this.baseDao.findById(req.params.id)
+      .then(pizza => {
+        res.status(200).send(pizza);
+      })
+      .catch(err => {
+        res.status(500).send(`Error retrieving pizza with id ${req.params.id}: ${err}`);
+      });
+  }
 }
--- src/pizza/index.ts
+++ src/pizza/index.ts
@@ -1,8 +1,15 @@
 import express from "express";
-import * as controller from "./controller";
+import { default as Pizza, PizzaModel } from "./model";
+import { MongoBaseDao } from "../dao";
+import { PizzaController } from "./controller";
 
 const router = express.Router();
 
-router.get("/", controller.findAll);
+const baseDao = new MongoBaseDao<PizzaModel>(Pizza);
+const controller = new PizzaController(baseDao);
+
+router.get("/", controller.getAllPizzas);
+
+router.get("/:id", controller.getPizzaById);
 
 export default router;
