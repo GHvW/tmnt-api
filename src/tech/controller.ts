7b34f5bdc6c968ccf11a84fd6bfc6e8e3a2796cb
ghvw <ghvanw283@gmail.com>
2018-10-14 00:16:08 -0500

    update tech and vehicle controllers

--- src/tech/controller.ts
+++ src/tech/controller.ts
@@ -1,11 +1,40 @@
-import { Response } from "express";
-import { default as Tech, TechModel } from "./model";
+import { Request, Response } from "express";
+import { TechModel } from "./model";
+import { IBaseDao } from "src/dao";
 
-export let findAll = (res: Response) => {
-  Tech.find({}, (err, techs: TechModel[]) => {
-    if (err) {
-      res.status(500).send(`Error retreiving techs. Error: ${err}`);
-    }
-    res.status(200).send(techs);
-  })
+// export let findAll = (res: Response) => {
+//   Tech.find({}, (err, techs: TechModel[]) => {
+//     if (err) {
+//       res.status(500).send(`Error retreiving techs. Error: ${err}`);
+//     }
+//     res.status(200).send(techs);
+//   })
+// }
+
+export class TechController {
+  baseDao: IBaseDao<TechModel>;
+
+  constructor(baseDao: IBaseDao<TechModel>) {
+    this.baseDao = baseDao;
+  }
+
+  getAllTechs(res: Response): void {
+    this.baseDao.findAll()
+      .then(techs => {
+        res.status(200).send(techs);
+      })
+      .catch(err => {
+        res.status(500).send(`Error retrieving techs: ${err}`);
+      });
+  }
+
+  getTechById(req: Request, res: Response): void {
+    this.baseDao.findById(req.params.id)
+      .then(techs => {
+        res.status(200).send(techs);
+      })
+      .catch(err => {
+        res.status(500).send(`Error retrieving tech with id ${req.params.id}: ${err}`);
+      });
+  }
 }
--- src/vehicle/controller.ts
+++ src/vehicle/controller.ts
@@ -1,11 +1,40 @@
-import { Response } from "express";
-import { default as Vehicle, VehicleModel } from "./model";
+import { Request, Response } from "express";
+import { VehicleModel } from "./model";
+import { IBaseDao } from "src/dao";
 
-export let findAll = (res: Response) => {
-  Vehicle.find({}, (err, vehicles: VehicleModel[]) => {
-    if (err) {
-      res.status(500).send("");
-    }
-    res.status(200).send(vehicles);
-  })
+// export let findAll = (res: Response) => {
+//   Vehicle.find({}, (err, vehicles: VehicleModel[]) => {
+//     if (err) {
+//       res.status(500).send("");
+//     }
+//     res.status(200).send(vehicles);
+//   })
+// }
+
+export class VehicleController {
+  baseDao: IBaseDao<VehicleModel>;
+
+  constructor(baseDao: IBaseDao<VehicleModel>) {
+    this.baseDao = baseDao;
+  }
+
+  getAllVehicles(res: Response): void {
+    this.baseDao.findAll()
+      .then(vehicles => {
+        res.status(200).send(vehicles);
+      })
+      .catch(err => {
+        res.status(500).send(`Error retrieving vehicles: ${err}`);
+      });
+  }
+
+  getVehicleById(req: Request, res: Response): void {
+    this.baseDao.findById(req.params.id)
+      .then(vehicle => {
+        res.status(200).send(vehicle);
+      })
+      .catch(err => {
+        res.status(500).send(`Error retrieving vehicle with id ${req.params.id}: ${err}`);
+      });
+  }
 }
