ca416ee0dbee28fe2344e6ea233968655be2f90b
ghvw <ghvanw283@gmail.com>
2018-10-13 19:37:50 -0500

    add base dao remove routes file

---
+++ src/dao.ts
@@ -0,0 +1,38 @@
+import mongoose, { Error } from "mongoose";
+
+
+export interface IBaseDao<T> {
+  findAll(): Promise<Array<T>>;
+  findById(id: string): Promise<T>;
+}
+
+
+export class BaseDao<T extends mongoose.Document> implements IBaseDao<T> {
+  model: mongoose.Model<mongoose.Document>;
+
+  constructor(model: mongoose.Model<mongoose.Document>) {
+    this.model = model;
+  }
+
+  findAll(): Promise<Array<T>> {
+    return new Promise((resolve, reject) => {
+      this.model.find({}, (err: Error, docs: Array<T>) => {
+        if (err) {
+          reject(err);
+        }
+        resolve(docs);
+      });
+    })
+  }
+
+  findById(id: string): Promise<T> {
+    return new Promise((resolve, reject) => {
+      this.model.findById(id, (err: Error, doc: T) => {
+        if (err) {
+          reject (err);
+        }
+        resolve(doc);
+      })
+    });
+  }
+}
