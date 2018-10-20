8d60c6810c6ae00a45da9346b6cf4bcf37355034
ghvw <ghvanw283@gmail.com>
2018-10-20 12:05:21 -0500

    change model class add function versions

---
+++ src/baseController.ts
@@ -0,0 +1,25 @@
+import { Request, Response } from "express";
+
+const getAllHandler = <T>(dbFn: () => Promise<T[]>) => {
+  return (res: Response) => {
+    dbFn()
+      .then(data => {
+        res.status(200).send(data);
+      })
+      .catch(err => {
+        res.status(500).send(`Error retrieving data: ${err}`);
+      });
+  }
+}
+
+const getByIdHandler = <T>(dbFn: (id: string) => Promise<T | null>) => {
+  return (req: Request, res: Response) => {
+    dbFn(req.params.id)
+      .then(item => {
+        res.status(200).send(item);
+      })
+      .catch(err => {
+        res.status(500).send(`Error retrieiving item with id ${req.params.id}: ${err}`);
+      });
+  }
+}
--- src/dao.ts
+++ src/dao.ts
@@ -1,38 +1,58 @@
-import mongoose, { Error } from "mongoose";
+import { Document, Model } from "mongoose";
 
 
 export interface IBaseDao<T> {
   findAll(): Promise<Array<T>>;
-  findById(id: string): Promise<T>;
+  findById(id: string): Promise<T | null>;
 }
 
 
-export class MongoBaseDao<T extends mongoose.Document> implements IBaseDao<T> {
-  model: mongoose.Model<mongoose.Document>;
+export class MongoBaseDao<T extends Document> implements IBaseDao<T> {
+  model: Model<T>;
 
-  constructor(model: mongoose.Model<mongoose.Document>) {
+  constructor(model: Model<T>) {
     this.model = model;
   }
 
+  // findAll(): Promise<Array<T>> {
+  //   return new Promise((resolve, reject) => {
+  //     this.model.find({}, (err: Error, docs: Array<T>) => {
+  //       if (err) {
+  //         reject(err);
+  //       }
+  //       resolve(docs);
+  //     });
+  //   });
+  // }
   findAll(): Promise<Array<T>> {
-    return new Promise((resolve, reject) => {
-      this.model.find({}, (err: Error, docs: Array<T>) => {
-        if (err) {
-          reject(err);
-        }
-        resolve(docs);
-      });
-    });
+    return this.model.find({}).exec();
   }
 
-  findById(id: string): Promise<T> {
-    return new Promise((resolve, reject) => {
-      this.model.findById(id, (err: Error, doc: T) => {
-        if (err) {
-          reject (err);
-        }
-        resolve(doc);
-      });
-    });
+  // findById(id: string): Promise<T> {
+  //   return new Promise((resolve, reject) => {
+  //     this.model.findById(id, (err: Error, doc: T) => {
+  //       if (err) {
+  //         reject (err);
+  //       }
+  //       resolve(doc);
+  //     });
+  //   });
+  // }
+  findById(id: string): Promise<T | null> {
+    return this.model.findById(id).exec();
   }
 }
+
+//mongoose findAll func, returns Promise<T[]>
+// const findAll = <T extends Document>(model: Model<T>) => {
+//   return () => {
+//     return model.find({}).exec();
+//   }
+// }
+
+//mongoose findById func, returns Promise<T | null>
+// const findById = <T extends Document>(model: Model<T>) => {
+//   return (id: string) => {
+//     return model.findById(id).exec();
+//   }
+// }
